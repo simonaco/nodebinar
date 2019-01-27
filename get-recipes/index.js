803e74d67f57c5f13ed8c0f6194a60c8f09f2493
Simona Cotin <simona.cotin@gmail.com>
2019-01-27 12:36:53 +0000

    🔨 fix context not found exception

--- get-recipes/index.js
+++ get-recipes/index.js
@@ -8,27 +8,30 @@ const loadDB = async () => {
   if (db) {
     return db;
   }
-  try {
-    const client = await MongoClient.connect(
-      process.env.url,
-      {
-        auth: auth
-      }
-    );
-    db = client.db('admin');
-  } catch (error) {
-    context.log(`Error code: ${error.code} message: ${error.message}`);
-    throw new Error(error.message);
-  }
+  const client = await MongoClient.connect(
+    process.env.url,
+    {
+      auth: auth
+    }
+  );
+  db = client.db('admin');
   return db;
 };
-module.exports = async function(context, req) {
-  const database = await loadDB();
-  let recipes = await database
-    .collection('Recipes')
-    .find()
-    .toArray();
-  context.res = {
-    body: { items: recipes }
-  };
+module.exports = async function(context) {
+  try {
+    const database = await loadDB();
+    let recipes = await database
+      .collection('Recipes')
+      .find()
+      .toArray();
+    context.res = {
+      body: { items: recipes }
+    };
+  } catch (error) {
+    context.log(`Error code: ${error.code} message: ${error.message}`);
+    context.res = {
+      status: 500,
+      body: { message: 'An error has occured, please try again later' }
+    };
+  }
 };
